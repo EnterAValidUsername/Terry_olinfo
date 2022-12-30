b64cfba3dbc1d19942aa5115b469dde93dcd0b71
EnterAValidUsername <tommopagano@icloud.com>
2022-12-30 13:30:33 +0100

    gitgnore + calcio_alpha

--- calcio/terry_calcio.cpp
+++ calcio/terry_calcio.cpp
@@ -1,14 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-ifstream fin ("input.txt");
+ifstream fin ("calcio_input_1.txt");
 ofstream fout ("output.txt");
 
 int main () {
+	
+	// va ma è troppo lento
+	
 	int T;
 	fin >> T;
 	
-	for (int t = 0; t < T; t++) {
+	for (int t = 1; t <= T; t++) {
 		int N, M, K, A, B;
 		fin >> N >> M >> K >> A >> B;
 		
@@ -29,10 +32,28 @@ int main () {
 			grid[x][y]++;
 		}
 		
+		// - - - - - - - - - - - - - - - - - - -
 		
-		//
+		/*
+		l'idea è quella di inserire una matrice AxB nella matrice NxM la quale parta dalla cella 0,0 di NxM;
+		cercare poi se è vantagggioso spostarla di 1 colonna a destra piuttosto che di una riga in giù finché
+		non sarà più conveniente fare spostamenti.
 		
-		cout << "case #" << t << ": " /*<< ans*/ << "\n";
+		problemi: ponendo il caso che ci sia un aagglomerato denso di alberi in un punto della foresta il quale 
+		impedisca di spostare vantaggiosamente AxB ma che la soluzione sia dopo questo agglomerato allora
+		la soluzione fallirebbe; tuttavia è possibile controllare le soluzioni dai 4 angoli, questo però invece che richiedere
+		N * M * (tot1*A + tot2*B), ne richiederebbe il quadruplo
+		*/
+		
+		int x = 0, y = 0; // indici della prima cella della matrice AxB
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				
+			}
+		}
+		
+		cout << "case #" << t << ": " << ans << "\n";
 	}
 
 return 0;
