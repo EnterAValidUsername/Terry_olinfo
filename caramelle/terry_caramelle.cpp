7b92d5c99ab3147a173d09229db043c9d8fb59d4
EnterAValidUsername <tommopagano@icloud.com>
2023-01-07 19:05:18 +0100

    Caramelle risolto
    ho studiato l'algoritmo per trovare i numeri primi ed è stata una cavolata ma non ci sarei mai arrivato da solo

--- caramelle/terry_caramelle.cpp
+++ caramelle/terry_caramelle.cpp
@@ -1,59 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-ifstream fin ("input.txt");
+#define ll long long int
+
+ifstream fin ("caramelle_input_1.txt");
 ofstream fout ("output.txt");
 
-int main () {
+map < long long int, int > fact (long long int n) {
+	map < long long int, int > facts;
 	
-	// this is the best i could do, it returns 3221225620
+	for (long long int i = 2; i*i <= n; i++) {
+		while (n % i == 0) {
+			if (facts.count(i) == -1) {
+				facts.insert(pair < long long int, int > (i, 1));
+			}
+			else {
+				facts[i]++;
+			}
+			n /= i;
+		}
+	}
+	
+	if (n > 1) facts.insert(pair < long long int, int > (n, 1));
+	return facts;
+}
+
+int main () {
 	
 	int T;
 	fin >> T;
 	
-	for (int t = 0; t < T; t++) {
-		int N;
-		fin >> N;
+	for (int t = 1; t <= T; t++) {
+		int n;
+		fin >> n;
 		
-		vector < pair < long long int, bool > > a(N);
+		long long int a[n];
 		
-		for (int i = 0; i < N; i++) {
-			fin >> a[i].first;
-			a[i].second = true;
+		for (int i = 0; i < n; i++) {
+			fin >> a[i];
 		}
 		
-		long long int ans = 1;
+		map < long long int, int > facts, temp;
 		
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < n; i++) {
+			temp = fact(a[i]);
 			
-			if (a[i].second || a[i].first != 1) {
-				
-				for (int j = 0; j < N; j++) {
-					
-					if (a[j].second || a[j].first != 1 || a[j].first != a[i].first) {
-						
-						if (a[i].first % a[j].second == 0) {
-							a[j].second = false;
-							
-							for (int k = 0; k < N; k++) {
-								if (a[k].second && a[k].first % a[j].first == 0) {
-									a[k].first /= a[j].first;
-								}
-							}
-						}
-					}
+			for (map < long long int, int > :: iterator j = temp.begin(); j != temp.end(); j++) {
+				if (facts.count(j -> first) >= 1) {
+					facts[j -> first] = max(facts[j -> first], temp[j -> first]);
+				}
+				else {
+					facts.insert(pair < long long int, int > (j -> first, 1));
 				}
 			}
 		}
 		
-		for (int i = 0; i < N; i++) {
-			if (a[i].second) {
-				ans *= a[i].second;
-			}
+		unsigned long long int ans = 1;
+		map < long long int, int > :: iterator it;
+		
+		for (it  = facts.begin(); it != facts.end(); it++) {
+			ans *= pow(it -> first, it -> second);
 		}
 		
 		fout << "case #" << t << ": " << ans << "\n";
-		
 	}
 	
 	return 0;
